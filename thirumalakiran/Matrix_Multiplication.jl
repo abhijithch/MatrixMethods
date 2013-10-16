c5c8c345ab41838f75207eafa819561d0117d9d1
Thirumala Kiran <thirumala.kiran@gmail.com>
2013-10-16 21:08:29 +0530

    Update Matrix_Multiplication.jl

--- thirumalakiran/Matrix_Multiplication.jl
+++ thirumalakiran/Matrix_Multiplication.jl
@@ -1,82 +1,89 @@
-using Winston
+    using Winston
 
-function matmul()
-    
-    x = [1:10]
-    y = [1:10]
-    x = convert(Array{Float64,1}, x)
-    y1 = convert(Array{Float64,1}, y)
-    y2 = convert(Array{Float64,1}, y)
-    y3 = convert(Array{Float64,1}, y)
-    y4 = convert(Array{Float64,1}, y)
-    
-    for i = 1: 10        
-        m = i * 200
-        n = i * 200
-        k = i * 200
-        A = rand(m,k)
-        B = rand(k,n)
-        C = zeros(m,n)
+    function matmul(val)
+        
+        x = convert(Array{Float64,1}, [1:10])
+        y1 = zeros(Float64,10)
+        y2 = zeros(Float64,10)
+        y3 = zeros(Float64,10)
+        y4 = zeros(Float64,10)
+        
+        for i = 1 : 10        
+            m = n = k = i * val    
+            A = rand(m,k)
+            B = rand(k,n)
+                        
 
-        #column wise and vectorized access
-	b = time()
-        for r = 1:k
-            C = C + A[:,k] * B[k,:]
+            #column wise and vectorized access
+            CWVA = zeros(m,n)
+    	    b = time()
+            for r = 1:k
+                CWVA = CWVA + A[:,r] * B[r,:]
+            end
+            a = time()
+            y1[i] = a-b
+                 
+            #column wise and non-vectorized access
+            CWNVA = zeros(m,n)
+    	    b = time()
+            for r = 1:k
+    	     for p = 1:m
+    	      	for q = 1:n
+    	            CWNVA[p,q] = CWNVA[p,q] + A[p,r] * B[r,q]
+    	        end
+    	     end
+            end
+            a = time()
+    	    y2[i] = a-b            
+            
+            #row wise and vectorized access
+            RWVA = zeros(m,n)
+    	    b = time()
+            for p = 1:m
+    	      for q = 1:n
+    	            RWVA[p,q] = (A[p,:] * B[:,q])[1]
+    	      end        
+    	    end
+            a = time()
+            y3[i] = a-b
+
+            
+            #row wise and non-vectorized access
+        	RWNVA = zeros(m,n)
+            b = time()    
+    	    for p = 1:m
+    	      for q = 1:n
+    		      for r = 1:k
+    		            RWNVA[p,q] = RWNVA[p,q] + A[p,r] * B[r,q]
+    		      end
+    	      end
+            end
+            a = time()
+    	    y4[i] = a-b
+
+                    
         end
-        a = time()
-        y1[i] = a-b
+        
+        #Plot the observastions
+        p = FramedPlot()        
+        setattr(p, "xlabel", string("Input size = x * ",val))
+        setattr(p, "ylabel", "Timetaken in seconds")
+        
+        
+        y1curve = Curve(x, y1,"color", "red")
+        setattr(y1curve, "label", "column wise and vectorized access")
 
- 	C = zeros(m,n)
-        #column wise and non-vectorized access
-	b = time()
-        for r = 1:k
-	     for p = 1:m
-	      	for q = 1:n
-	            C[p,q] = C[p,q] + A[p,k] * B[k,q]
-	        end
-	     end
-        end
-        a = time()
-	y2[i] = a-b
+        y2curve = Curve(x, y2,"color", "blue")
+        setattr(y2curve, "label", "column wise and non-vectorized access")
 
-        C = zeros(m,n)
-        #row wise and vectorized access
-	b = time()
-        for p = 1:m
-	      for q = 1:n
-	            C[p,q] = (A[p,:] * B[:,q])[1]
-	      end        
-	end
-        a = time()
-        y3[i] = a-b
+        y3curve = Curve(x, y3,"color", "green")
+        setattr(y3curve, "label", "row wise and vectorized access")
 
-        C = zeros(m,n)
-        #row wise and non-vectorized access
-    	b = time()    
-	for p = 1:m
-	      for q = 1:n
-		      for r = 1:k
-		            C[p,q] = C[p,q] + A[p,k] * B[k,q]
-		      end
-	      end
-        end
-        a = time()
-	y4[i] = a-b
+        y4curve = Curve(x, y4,"color", "brown")
+        setattr(y4curve, "label", "row wise and non-vectorized access")
 
+        l = Legend( .1, .9, {y1curve, y2curve, y3curve, y4curve} )
+        add(p, y1curve, y2curve, y3curve, y4curve, l)          
+        Winston.display(p)
+        file(p, "matrix_multiplication.png")
     end
-    
-    #Plot the observastions
-    p = FramedPlot()
-    setattr(p, "title", "col vec in red vs col in blue vs row vec in green vs row in brown")
-    setattr(p, "xlabel", "exp no. (input size = exp no * 100)")
-    setattr(p, "ylabel", "timetaken in seconds")
-    add(p, Curve(x, y1,"color", "red"))  #column wise and vectorized access
-    add(p, Curve(x, y2,"color", "blue")) #column wise and non-vectorized access
-    add(p, Curve(x, y3,"color", "green")) #row wise and vectorized access
-    add(p, Curve(x, y4,"color", "brown")) #row wise and non-vectorized access
-    Winston.display(p)
-    file(p, "matrix_multiplication.png")
-end
-
-#Find the plot here - https://docs.google.com/file/d/0B3nPBIEtlfPGdTlIM1c3WHFZS1k/edit?usp=sharing
-#row wise and non-vectorized access took less time to complete the operation
